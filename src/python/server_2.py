28a7791ba25cf95cde7ed2604668be5630b7d8b3
qitas <qitas@qitas.cn>
2019-11-22 11:41:01 +0800

    fix python2

--- src/python/server_2.py
+++ src/python/server_2.py
@@ -1,51 +1,18 @@
-#coding:utf-8
-
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 '''
 建立一个python server，监听指定端口，
 如果该端口被远程连接访问，则获取远程连接，然后接收数据，
 并且做出相应反馈。
 '''
 
-#import necessary package
 import socket
 import time
 import sys
 
-HOST_IP = "172.18.250.47"
+HOST_IP = "182.61.61.133"
 HOST_PORT = 17799
-host_addr = (HOST_IP, HOST_PORT)
-'''
-print("Starting socket: TCP...")
-#1.create socket object:socket=socket.socket(family,type)
-socket_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("TCP server listen @ %s:%d!" %(HOST_IP, HOST_PORT) )
-host_addr = (HOST_IP, HOST_PORT)
-#2.bind socket to addr:socket.bind(address)
-socket_tcp.bind(host_addr)
-#3.listen connection request:socket.listen(backlog)
-socket_tcp.listen(1)
-#4.waite for client:connection,address=socket.accept()
-socket_con, (client_ip, client_port) = socket_tcp.accept()
-print("Connection accepted from %s." %client_ip)
-socket_con.send("Welcome to RPi TCP server!")
-
-print("Receiving package...")
-while True:
-    try:
-        data=socket_con.recv(512)
-        if len(data)>0:
-            print("Received:%s"%data)
-            if data=='1':
-                GPIO.output(11,GPIO.HIGH)
-            elif data=='0':
-                GPIO.output(11,GPIO.LOW)
-            socket_con.send(data)
-            time.sleep(1)
-            continue
-    except Exception:
-        socket_tcp.close()
-        sys.exit(1)
-'''
+host_addr = ("", HOST_PORT)
 		
 try: 
 	print "Server is starting"
@@ -88,7 +55,7 @@ try:
 		print "closing one connection" #当一个连接监听循环退出后，连接可以关掉
 		connection.close()  
 		
-except Exception:
+except Exception,err:
  	print "Can't server,try it latter!"
 	sys.exit(1)
 
